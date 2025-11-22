8d90db009e65d8447fd532385c1ff9aa64837841
Karan09542 <karan09542@gmail.com>
2025-11-22 07:28:02 +0530

    updated once

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -7,13 +7,13 @@ const ListItem = () => {
         <h1>List Item</h1>
         <ul>
             <li>
-                <Link to="/item/1">Item 1</Link>
+                <Link to="/items/1">Item 1</Link>
             </li>
             <li>
-                <Link to="/item/2">Item 2</Link>
+                <Link to="/items/2">Item 2</Link>
             </li>
             <li>
-                <Link to="/item/3">Item 3</Link>
+                <Link to="/items/3">Item 3</Link>
             </li>
         </ul>
     </div>
